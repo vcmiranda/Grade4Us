dd0f0a80f64f4af55b465b16caff8264db50803b
Vitor Miranda <vcmiranda@outlook.com>
2018-03-25 16:15:52 -0400

    Updated packages. Created date conversion functions.

--- client/utils/moment.utils.js
+++ client/utils/moment.utils.js
@@ -1,7 +1,20 @@
-const moment = {
-  convertToCalendar() {
-    return 0;
+import moment from 'moment';
+
+const convert = {
+  convertDate(date) {
+    if (!date) {
+      return null;
+    } else if (/^\d+$/.test(date)) {
+      return moment(date, 'X').format('YYYY-MM-DD');
+    }
+    return moment(date, 'YYYY-MM-DD').format('X');
+  },
+  convertInverseToFormattedDate(date) {
+    if (!date) {
+      return null;
+    }
+    return moment(date, 'YYYY-MM-DD').format('MMM DD, YYYY');
   },
 };
 
-export default moment;
+export default convert;
